0ccab041d5068108f4af3c92f4e1e4fddfa80cd1
csa <csxeba@gmail.com>
2017-02-13 14:41:50 +0000

    Separated connection logic from messaging logic

--- serverapp.py
+++ serverapp.py
@@ -6,58 +6,58 @@ from tkinter import messagebox as tkmb
 
 from application import Application
 
-HIM = "127.0.0.1"
+HER = "127.0.0.1"
+ME = "127.0.0.1"
 
 
-class MsgClient(Application):
+class MsgServer(Application):
 
     def __init__(self):
-        super(MsgClient, self).__init__()
-        self.title("Kliens")
+        super(MsgServer, self).__init__()
+        self.title("Szerver")
 
-        self.msocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.msocket.settimeout(1)
+        self.csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.csocket.settimeout(3)
+        self.csocket.bind((ME, 12345))
+        self.msocket = None
 
         self.connector = thr.Thread(target=self.connect)
         self.listener = thr.Thread(target=self.listen)
         self.sender = thr.Thread(target=self.send_messages)
 
-        self.connector.start()
-
     def connect(self):
         self.set_label("Várakozás kapcsolatra...")
+        self.csocket.listen(1)
         while self.running:
             try:
-                self.msocket.connect((HIM, 12345))
-            except ConnectionRefusedError:
-                time.sleep(1)
+                self.msocket, addr = self.csocket.accept()
             except socket.timeout:
                 pass
             else:
-                self.set_label("Kapcsolat felépítve")
+                if addr[0] != HER:
+                    tkmb.showwarning("Ajjaj", f"Bejövő cím: {HER} helyett {addr[0]}")
+                else:
+                    print(f"Connection from {addr}")
                 break
         else:
             print("Connector nice exit!")
             return
 
+        self.msocket.settimeout(1)
         while self.pwhash is None:
             time.sleep(1)
-        self.msocket.sendall(self.pwhash + b"ROGER")
 
-        msg = b""
-        while msg[-5:] != b"ROGER":
+        hsh = b""
+        while hsh[-5:] != b"ROGER":
             try:
-                msg += self.msocket.recv(1024)
+                hsh += self.msocket.recv(1024)
             except socket.timeout:
                 pass
-            time.sleep(0.1)
-        if msg[:-5] == b"SODOFF":
-            tkmb.showerror("Autentikációs hiba!", "Helytelen jelszó! Leállás...")
+        if hsh[:-5] != self.pwhash:
+            tkmb.showerror("Autentikációs hiba!")
             self.teardown()
             return
-        else:
-            self.display.insert("end", "\nAutentikálva!")
-            self.label.configure(text="Autentikálva")
+        self.msocket.sendall(b"OKROGER")
 
         self.connected = True
         self.listener.start()
@@ -66,6 +66,6 @@ class MsgClient(Application):
 
 
 if __name__ == '__main__':
-    app = MsgClient()
+    app = MsgServer()
     app.mainloop()
     print("Application nice exit!")
